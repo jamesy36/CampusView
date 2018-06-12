0f0976f41379ed78b71e9df0e3a02c57e83f57e6
Ashlynn Cardoso <ashlynncardoso@gmail.com>
2018-06-11 19:32:24 -0700

    On click nearby shows current location with a marker

--- app/src/main/java/com/example/ashlynncardoso/campusview/MainActivity.java
+++ app/src/main/java/com/example/ashlynncardoso/campusview/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.ashlynncardoso.campusview;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button getLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getLocation = (Button) findViewById(R.id.loc_button);
+        getLocation.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                nextButton();
+            }
+        });
+    }
+
+    private void nextButton(){
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
